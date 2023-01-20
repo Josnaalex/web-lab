f35f68c94fe67bfad57da19c1a6f934f0b9fe28f
Josna M Alexander <84304028+Josnaalex@users.noreply.github.com>
2023-01-20 15:49:14 +0530

    Add files via upload

---
+++ qn_9/score.php
@@ -0,0 +1,44 @@
+<html>
+<body>
+<form method="post" action="score.php">
+	<label>ktu id:</label>
+	<input type="text" name="id"><br>
+	<label>NAME:</label>
+	<input type="text" name="name"><br>
+	<label>series 1:</label>
+	<input type="text" name="s1"><br>
+	<label>series 2</label>
+	<input type="text" name="s2"><br>
+	<label>assignment 1:</label>
+	<input type="text" name="a1"><br>
+	<label>assignment 2:</label>
+	<input type="text" name="a2"><br>
+	<label>ATTENDANCE:</label>
+	<input type="text" name="att"><br>
+	<input type="submit" value="add"name="s">
+</form>
+</body>
+</html>
+<?php
+include("connection.php");
+if(isset($_POST['s']))
+{
+$i=$_POST['id'];
+$n=$_POST['name'];
+$so=$_POST['s1'];
+$ss=$_POST['s2'];
+$ao=$_POST['a1'];
+$as=$_POST['a2'];
+$att=$_POST['att'];
+$k=mysqli_query($conn,"INSERT INTO `student`(`ktuid`, `name`, `series1`, `series2`, `assi1`, `assi2`, `attendance`) VALUES ('$i','$n','$so','$ss','$ao','$as','$att')");
+if($k)
+{
+echo("inserted successfully");
+}
+}
+
+
+
+?>
+
+
